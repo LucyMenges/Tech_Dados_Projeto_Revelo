c1345c9b403068bc7663ffec512538f38545739b
Raphael Cespedes <102712353+nhqlus@users.noreply.github.com>
2022-11-21 22:43:27 -0300

    add melhorias e funcoes

--- gt_script.py
+++ gt_script.py
@@ -4,32 +4,34 @@ Created on Fri Nov 18 19:58:22 2022
 
 @author: rapha
 
-base: https://www.youtube.com/@Pythonenthusiast
+references:
+    https://trends.google.com/
+    https://pypi.org/project/pytrends/#api-methods
+    https://www.youtube.com/@Pythonenthusiast
+    https://lazarinastoy.com/the-ultimate-guide-to-pytrends-google-trends-api-with-python/
 """
 
-# pytrend precisa de algumas lib para rodar:
+# pytrend precisa das seguintes libs:
 '''
 pip install pytrends # Unofficial API for Google Trends.
 pip install requests # Requests is a simple, yet elegant, HTTP library.
 pip install lxml # As an XML library, lxml is often used under the hood of in-house server applications.
 pip install pandas
 '''
-
 from pytrends.request import TrendReq
-import pandas as pd
 
-## chamar a lib e setar os argumentos:
-# solicitar e definir a linguagem, 'en-US', 'pt-BR'.
-pytrends = TrendReq(hl='pt-BR', timeout=(30,60), retries=2)
+# para carregar os argumentos do payload, o payload vai ser usado para todas as funcoes da lib:
+# vamos definir a linguagem, 'en-US', 'pt-BR' e regra de conexao.
+pytrends = TrendReq(hl='pt-BR', timeout=(30, 60), retries=2)
 
-# montar lista de palavras que vamos analisar. Nomalmente o google trends limitaria em até 5 correlações entre as palavra, mas vamos expandir isso...
-all_keywords = ['SQL', 'Python',
-                'DBeaver', 'Power BI',
-                'Pandas', 'Data Analist']
+# vamos montar lista de palavras que vamos analisar. Nomalmente o google trends limitaria de 1 até 5 palavra, mas vamos expandir isso...
+keywords_list = ['SQL', 'Python',
+                 'Power BI', 'Pandas']
 
-dict_1 = {'word_ak':all_keywords, 'df_it':[], 'df_r':[], 'mean_df_it':[]}
+data_dict = {'df_keyword': keywords_list, 'df_it': [],
+             'df_ir': [], 'df_rt': [], 'df_rq': []}
 
-# qual o periodo de tempo que iremos buscar no passado.
+# vamos definir qual o periodo de tempo que iremos buscar no passado.
 timeframe = ['all', 'today 5-y', 'today 12-m',
              'today 3-m', 'today 1-m', 'now 7-d',
              'now 1-d']
@@ -43,38 +45,34 @@ geo = ['', 'BR', 'US']
 # se vamos buscar em algum lugar especifico dentro do google, como websearch (defaut), youtube, news e shopping..
 gprop = ''
 
+# para setar os agumento de interest_by_region
+# o nível que iremos buscar a informacao
+resolution = ['CITY', 'COUNTRY', 'REGION', 'DMA']
+
+# se vamos incluir, ou nao, volume pequenos por paises/regioes
+inc_low_vol = [True, False]
+
+# se vamos incluir, ou nao, o codigo ISO dos paises e o nome da data
+inc_geo_code = [True, False]
+
+
 def check_trends(kw):
-    # funcao para correr o interest_over_time
+    # o payload:
     pytrends.build_payload([kw], cat, timeframe[2], geo[1], gprop)
-    data = pytrends.interest_over_time()
-    # data2 = pytrends.interest_by_region(resolution='BRAZIL', inc_low_vol=True, inc_geo_code=False)
-    return data #(data, data2)
-
-# para correr cada palavra inserida na lista em separado 
-for kw in all_keywords:
-    data = check_trends(kw)
-    # data, data2 = check_trends(kw)
-    dict_1['df_it'].append(data)
-    # dict_1['df_r'].append(data2)
-
-for kdf in dict_1['df_it']:
-    dict_1['mean_df_it'].append(kdf.iloc[:0].mean())
+    # definindo funcoes da lib pytrends:
+    data_it = pytrends.interest_over_time()
+    data_ir = pytrends.interest_by_region(resolution[0],
+                                          inc_low_vol[1],
+                                          inc_geo_code[0])
+    data_rt = pytrends.related_topics()
+    data_rq = pytrends.related_queries()
+    return data_it, data_ir, data_rt, data_rq
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# para ciclar cada kw inserida na keywords_list, extrair resultado como df e inserir no data_dict
+for kw in keywords_list:
+    data_it, data_ir, data_rt, data_rq = check_trends(kw)
+    data_dict['df_it'].append(data_it)
+    data_dict['df_ir'].append(data_ir)
+    data_dict['df_rt'].append(data_rt)
+    data_dict['df_rq'].append(data_rq)
