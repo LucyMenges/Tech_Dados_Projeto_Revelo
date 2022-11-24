a9169b49131f8871673fa8bbafcf00fbe55bf75c
Raphael Cespedes <102712353+nhqlus@users.noreply.github.com>
2022-11-23 22:58:16 -0300

    Update aplicacao_principal.py

--- aplicacao_principal.py
+++ aplicacao_principal.py
@@ -12,7 +12,6 @@ for kw in keywords_list:
     data_it, data_ir, data_rt, data_rq = gt.check_trends(
         kw, cat='0', timeframe='today 12-m', geo='BR', gprop='',
         resolution='COUNTRY', inc_low_vol=False, inc_geo_code=True)
-
     data_dict['df_it'].append(data_it)
     data_dict['df_ir'].append(data_ir)
     data_dict['df_rt'].append(data_rt)
