a07a14812e532dbd30af016db5197edd499e065a
engenharia1 <113902909+engenharia1@users.noreply.github.com>
2022-12-07 11:30:05 -0300

    update documentação

--- keywords.py
+++ keywords.py
@@ -1,28 +1,4 @@
-# import ws_script as ws
-
-# # PARAMETROS 
-# # Criar o navegador, com o google Chrome atualizado.
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# servico = Service(ChromeDriverManager().install())
-# nav = webdriver.Chrome(service=servico)
-# nav.set_page_load_timeout(300)
-
-# # Link da página a ser aberta pelo navegador
-# pag1 = "https://www.vagas.com.br/"
-
-# # Cargo a ser pesquisado
-# cargos = ['Analista de Dados']
-
-# nr_pag = 2
-
-# # Transformando o resultado num DataFrame
-# df3 =  ws.busca_site_vagas(nav, pag1, cargos, nr_pag=2)
-
-# # Salvando o dataFrame final para CSV file
-# df3.to_csv("lista2_vagas_vagas_com.csv")
-
+# IMPORTAÇÃO DE BIBLIOTECAS
 import pandas as pd
 import nltk
 import re
@@ -31,9 +7,9 @@ from nltk.corpus import stopwords
 nltk.download('punkt')
 nltk.download('stopwords')
 from nltk.tokenize import word_tokenize
+from collections import Counter
 
-# UNINDO TODOS OS DF DE CADA LINK
-
+# UNIÃO DE TODOS OS DATAFRAMES DE CADA LINK
 lista_df = []
 
 for i in range(120):  
@@ -44,16 +20,15 @@ for i in range(120):
         lista_df.append(df)
 
 df_total = pd.concat(lista_df)
-
 df_total.to_csv('df_total_descr.csv')
-
 df_total = pd.read_csv(r'C:\Users\Engenharia R3\Documents\GitHub\Tech_Dados_Projeto_Revelo\df_total_descr.csv') 
 df_total = df_total.astype(str)
 
+# SEPARAÇÃO DAS STOP-WORDS E PRÉ-SELEÇÃO DE PALAVRAS A REMOVER DO DATAFRAME GERADO
 stop_words = stopwords.words('portuguese')
-
 removal_words = ['descrição', 'conhecimento', 'experiência', 'requisitos', 'área', 'outros', 'atividades', 'nível', 'áreas', 'desejável', 'informações', 'realizar', 'completo', 'empresa','através','fazer','principais','·','vida', '–','bem','novos','aqui','eou','time',]
 
+# FUNÇÃO PARA FAZER A LIMPEZA GERAL DA STRING NA COLUNA DE DESCRIÇÕES DAS VAGAS
 def cleaning(text):   
     # Converter para letra minúscula, Remover links URL, caracteres especiais, pontuações...
     text = text.lower()
@@ -67,8 +42,7 @@ def cleaning(text):
     for word in removal_words:
      text = re.sub(word,'', text) 
 
-
-    # Remover emojis (não será necessário) 
+    # # Remover emojis (não será necessário) 
     # emoji_pattern = re.compile("["
     #                        u"U0001F600-U0001F64F"  # emoticons
     #                        u"U0001F300-U0001F5FF"  # symbols & pictographs
@@ -87,19 +61,17 @@ def cleaning(text):
     
     return text
 
-# Filtrar as linhas que contém a string 'descrição:' 
+# FILTRAR AS LINHAS QUE CONTÉM A STRING 'DESCRIÇÃO:' E ADICIONANDO EM UMA TERCEIRA COLUNA 
 df_total.loc[df_total['descr2_vg'].str.contains('descrição:', case=False, na=False), "descrição_final"] =  df_total['descr2_vg']
 df_total.loc[df_total['descr3_vg'].str.contains('descrição:', case=False, na=False), "descrição_final"] =  df_total['descr3_vg']
 
-# Aplicar a função para limpar/filtrar as strings
+# APLICAR A FUNÇÃO PARA LIMPEZA/FILTRO DAS STRINGS
 dt1 = df_total['descrição_final'].apply(cleaning)
 
-# Contar as palavras que mais aparecem na coluna "descrição_final"
-from collections import Counter
+# CONTAGEM DE PALAVRAS QUE MAIS APARECEM NA COLUNA 'DESCRIÇÃO_FINAL'
 p1 = Counter(" ".join(dt1).split()).most_common(500)
-
 rslt = pd.DataFrame(p1, columns=['Word', 'Frequency'])
 print(rslt)
 
-# Salvando o dataFrame final para CSV file
+# SALVAR O DATAFRAME FINAL PARA CSV FILE
 rslt.to_csv("rslt1.csv")
