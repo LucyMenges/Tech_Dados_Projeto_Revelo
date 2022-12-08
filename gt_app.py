38de1dbda68137d376bdab1a1dbdefb1041b4d62
Raphael Cespedes <102712353+nhqlus@users.noreply.github.com>
2022-12-07 21:47:10 -0300

    une conteudo

--- gt_app.py
+++ gt_app.py
@@ -27,7 +27,7 @@ data_dict = {'keywords': keywords_list, 'df_it': [],
 for kw in keywords_list:
     data_it, data_ir, data_rt = gt.check_trends(
         kw, cat='0', timeframe='today 12-m', geo='BR', gprop='',
-        resolution='CITY', inc_low_vol=False, inc_geo_code=True)
+        resolution='CITY', inc_low_vol=True, inc_geo_code=True)
     data_dict['df_it'].append(data_it)
     data_dict['df_ir'].append(data_ir)
     data_dict['df_rt'].append(data_rt)
--- une_conjuntos.py
+++ une_conjuntos.py
@@ -7,12 +7,12 @@ Created on Wed Dec  7 11:37:08 2022
 import pandas as pd
 import numpy as np
 
-diretorio = 'C:/Users/llanz/OneDrive/Documents/GitHub/Tech_Dados_Projeto_Revelo/data_Ferramentas/'
+diretorio = 'C:/_dev/residencia_harve/Tech_Dados_Projeto_Revelo/data_Habilidades/'
 
-keywords_list = ['Excel', 'Power BI',  'Banco de Dados', 'Pacote Office',
-                 'SQL', 'SAP', 'Python', 'Cloud', 'CRM', 'Protheus', 'Analytics',
-                 'Oracle', 'R', 'Azure', 'Salesforce', 'Linux']  
-
+keywords_list = ['Inglês', 'Equipe', 'Responsável', 'Comunicação', 'Planejamento', 'Modelagem', 'Foco',
+ 'Estatística', 'Documentação', 'Inovação', 'Metodologias Ágeis', 'Decisão', 'Prazos', 'Extração de Dados', 
+ 'Machine Learning','Indicadores', 'Dashboards', 'Logística', 'Marketing', 'Implementação',  
+ 'ETL', 'Visão', 'Inteligência Artificial', 'Inteligência de Negócio', 'TI', 'Coleta de Dados', 'Fluxos']  
 
 lista_data_ir = []
 lista_data_it = []
@@ -21,23 +21,23 @@ lista_data_rt = []
 for kw in keywords_list:
     
     df_ir = pd.read_csv(diretorio + 'data_ir_' +str([kw]), sep=',')
-    df_ir['ferramenta'] = np.repeat(df_ir.columns[2], df_ir.shape[0]) 
-    df_ir.columns = ['geoname', 'geocode', 'quantidade', 'ferramenta']
+    df_ir['habilidades'] = np.repeat(df_ir.columns[2], df_ir.shape[0]) 
+    df_ir.columns = ['geoname', 'geocode', 'quantidade', 'habilidades']
     lista_data_ir.append(df_ir)
     
     df_it = pd.read_csv(diretorio + 'data_it_' +str([kw]), sep=',')
-    df_it['ferramenta'] = np.repeat(df_it.columns[1], df_it.shape[0]) 
-    df_it.columns = ['date', 'quantidade', 'isPartial', 'ferramenta']
+    df_it['habilidades'] = np.repeat(df_it.columns[1], df_it.shape[0]) 
+    df_it.columns = ['date', 'quantidade', 'isPartial', 'habilidades']
     lista_data_it.append(df_it)
     
     df_rt = pd.read_csv(diretorio + 'data_rt_' +str([kw]), sep=',')
-    df_rt['ferramenta'] = np.repeat(kw, df_rt.shape[0])
+    df_rt['habilidades'] = np.repeat(kw, df_rt.shape[0])
     lista_data_rt.append(df_rt)
     
     
 df_ir_total = pd.concat(lista_data_ir, axis = 0)
-df_ir_total.to_csv('df_ir_total_ferram.csv')
+df_ir_total.to_csv('df_ir_total_hab.csv')
 df_it_total = pd.concat(lista_data_it, axis = 0)
-df_it_total.to_csv('df_it_total_ferram..csv')
+df_it_total.to_csv('df_it_total_hab.csv')
 df_rt_total = pd.concat(lista_data_rt, axis = 0)
-df_rt_total.to_csv('df_rt_total_ferram..csv')
+df_rt_total.to_csv('df_rt_total_hab.csv')
